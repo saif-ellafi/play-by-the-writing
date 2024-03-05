2f29856822a9fe39c89d2a19b5daa397893f8cd2
Saif Addin <saif1988@gmail.com>
2024-03-05 21:12:45 +0100

    Bump tables revision

--- scripts/playbtw_common.py
+++ scripts/playbtw_common.py
@@ -33,7 +33,7 @@ for folder in empty_user_folders:
     path = pbwdir_path(folder)
     os.makedirs(path, exist_ok=True)
 
-TREV = 1000
+TREV = 1001
 
 # Record trev in hidden file.
 trev_path = pbwdir_path('.pbtwtrev')
